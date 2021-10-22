c5eaefa3c9935495bc71bef3e5ca4bd5d64b7342
vmgn16 <mg_619@hotmail.com>
2021-10-21 19:49:20 -0500

    uso de los listados con for

--- app/views.py
+++ app/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 def home(request):
-    return render(request, 'app/home.html')
+    productos = Producto.objects.all()
+    data= {
+        'productos': productos
+
+    }
+    return render(request, 'app/home.html', data)
 
 def contacto(request):
     return render(request, 'app/contacto.html')
 
 def galeria(request):
     return render(request, 'app/galeria.html')
+
